126550e15fc9b9dab99c8de3c6d3cf801a0861b6
Mark Deeb <Mark.Deeb@quadrant.local>
2018-05-18 17:23:13 -0400

    Add project files.

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Models
+{
+    //database context is the main class that 
+    //coordinates Entity Framework functionality for a given data model
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
